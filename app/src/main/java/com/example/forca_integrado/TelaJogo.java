987d662dc3063789c5c005c51fd4f2343f930de9
elisamiglioranzi08 <elisa.miglioranzi08@gmail.com>
2026-05-05 14:20:23 -0300

    05/05/2026

--- app/src/main/java/com/example/forca_integrado/TelaJogo.java
+++ app/src/main/java/com/example/forca_integrado/TelaJogo.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,14 +13,18 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class TelaJogo extends AppCompatActivity implements View.OnClickListener {
 
     private ImageView imagem;
-    private ArrayList<Integer> listaImagens;
     private int indiceListaImagens;
-    private Arraylist<Integer> ListaImagens;
+    private Arraylist<Integer> ListaImagens, ListaIdsButtons;
+    private ArrayList<String> ListaPalavras;
     private Button b1;
+    private TextView texto;
+    private String palavra;
+    private char[] estado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,8 +54,77 @@ public class TelaJogo extends AppCompatActivity implements View.OnClickListener 
         b1 = findViewById(R.id.id1);
         b1.setOnClickListener(this);
 
+        ListaPalavras = new ArrayList<String>();
+        ListaPalavras.add("CAIXA");
+        ListaPalavras.add("SUPERMERCADO");
+        ListaPalavras.add("COMPUTADOR");
+        ListaPalavras.add("INSTITUTO");
+        ListaPalavras.add("CELEBRIDADE");
+        ListaPalavras.add("HIMALAIA");
+        ListaPalavras.add("FESTIVAL");
+        ListaPalavras.add("SHAKIRA");
+        ListaPalavras.add("COMPUTAÇÃO");
+        ListaPalavras.add("CORINTHIANS");
+
+        texto = findViewById(R.id.textView3);
+
+        ListaIdsButtons = new Arraylist<Integer>();
+        ListaIdsButtons.add(R.id.id1);
+        ListaIdsButtons.add(R.id.id2);
+        ListaIdsButtons.add(R.id.id3);
+        ListaIdsButtons.add(R.id.id4);
+        ListaIdsButtons.add(R.id.id5);
+        ListaIdsButtons.add(R.id.id6);
+        ListaIdsButtons.add(R.id.id7);
+        ListaIdsButtons.add(R.id.id8);
+        ListaIdsButtons.add(R.id.id9);
+        ListaIdsButtons.add(R.id.id10);
+        ListaIdsButtons.add(R.id.id11);
+        ListaIdsButtons.add(R.id.id12);
+        ListaIdsButtons.add(R.id.id13);
+        ListaIdsButtons.add(R.id.id14);
+        ListaIdsButtons.add(R.id.id15);
+        ListaIdsButtons.add(R.id.id16);
+        ListaIdsButtons.add(R.id.id17);
+        ListaIdsButtons.add(R.id.id18);
+        ListaIdsButtons.add(R.id.id19);
+        ListaIdsButtons.add(R.id.id20);
+        ListaIdsButtons.add(R.id.id21);
+        ListaIdsButtons.add(R.id.id22);
+        ListaIdsButtons.add(R.id.id23);
+        ListaIdsButtons.add(R.id.id24);
+        ListaIdsButtons.add(R.id.id25);
+        ListaIdsButtons.add(R.id.id26);
+
+        for (int j = 0; j<ListaIdsButtons.size(); j++){
+            Button b = findViewById(ListaIdsButtons.get(j));
+            b.setOnClickListener(this);
+        }
     }
 
+    public void inicializaJogo(){
+        imagem.setImageResource(R.drawable.forca_0_9);
+        palavra = sorteiaPalavra();
+        estado = new char[palavra.length()];
+        for(int i = 0; i<estado.length; i++) {
+            estado[i] = '_';
+        }
+        atualizaTexto();
+    }
+    public void atualizaTexto(){
+        String temporaria = new String();
+        temporaria = "";
+        for(int i = 0; i<estado.length; i++){
+            temporaria += estado[i] + " ";
+        }
+        texto.setText(temporaria);
+    }
+    public String sorteiaPalavra (){
+        String retorno = new String();
+        Collections.shuffle(ListaPalavras);
+        retorno = ListaPalavras.get(0);
+        return retorno;
+    }
     public void atualizaForca(){
         indiceListaImagens++;
         imagem.setImageResource(ListaImagens.get(indiceListaImagens));
@@ -58,6 +132,6 @@ public class TelaJogo extends AppCompatActivity implements View.OnClickListener 
 
     @Override
     public void onClick(View view) {
-        atualizaForca();
+        texto.setText(sorteiaPalavra());
     }
 }
